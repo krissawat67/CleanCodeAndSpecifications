5e9218821510688669100734345866ac13f73e7d
krissawat67 <krissawat.c@ku.th>
2026-07-10 17:53:01 +0700

    Delete CleanCodeAndSpecifications-solution directory

--- CleanCodeAndSpecifications-solution/src/lab/User.java
+++
@@ -1,7 +0,0 @@
-package lab;
-
-public class User {
-    public final boolean active;
-    public final int balance;
-    public User(boolean active, int balance) { this.active = active; this.balance = balance; }
-}
